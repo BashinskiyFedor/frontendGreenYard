7b9082a4bd79db78042be97a45635efa19473044
Fedor Bashinsky <fedorbashinsky@yandex.ru>
2020-10-25 00:05:24 +0300

    added vue-template

---
+++ src/components/main/greenhouse/Greenhouse.vue
@@ -0,0 +1,14 @@
+<template>
+<GreenhouseNavigate />
+<GreenhouseFooter />
+</template>
+
+<script lang="ts">
+import {
+    defineComponent
+} from 'vue';
+import GreenhouseNavigate from "../"
+export default defineComponent({
+    name: 'Greenhouse'
+})
+</script>
---
+++ src/components/main/greenhouse/includes/GreenhouseNavigate.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="pt-4 pb-5">
+    <div class="container">
+        <div class="row d-flex align-items-start align-items-center px-3 px-md-0">
+            <div class="col-md-4 d-flex mb-2 mb-md-0">
+                <a class="navbar-brand d-flex align-items-center" href="index.html">
+                    <span class="flaticon flaticon-crane"></span>
+                    <span class="ml-2">GreenHouse Yard<small>Construction Company</small></span>
+                </a>
+            </div>
+            <div class="col-md-4 d-flex topper mb-md-0 mb-2 align-items-center">
+                <div class="icon d-flex justify-content-center align-items-center">
+                    <span class="fa fa-map"></span>
+                </div>
+                <div class="pr-md-4 pl-md-3 pl-3 text">
+                    <p class="con"><span>Наш номер</span> <span>8-962-300-37-33</span></p>
+                    <p class="con">Позвоните нам сейчас 24/7 Служба поддержки клиентов</p>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex topper mb-md-0 align-items-center">
+                <div class="icon d-flex justify-content-center align-items-center"><span class="fa fa-paper-plane"></span>
+                </div>
+                <div class="text pl-3 pl-md-3">
+                    <p class="hr"><span>Наше местоположение</span></p>
+                    <p class="con">г. Белгород, ул. 45-Стрелковой дивизии</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container d-flex align-items-center">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span> Menu
+        </button>
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"><a href="index.html" class="nav-link">Главная</a></li>
+                <li class="nav-item"><a href="about.html" class="nav-link">О нас</a></li>
+                <li class="nav-item"><a href="services.html" class="nav-link">Услуги</a></li>
+                <li class="nav-item active"><a href="project.html" class="nav-link">Проекты</a></li>
+                <li class="nav-item"><a href="blog.html" class="nav-link">Блог</a></li>
+                <li class="nav-item"><a href="contact.html" class="nav-link">Связаться с нами</a></li>
+            </ul>
+            <a href="#" class="btn-custom" data-toggle="modal" data-target="#exampleModalCenter">Узнать больше</a>
+        </div>
+    </div>
+</nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "GreenHouseNavigate"
+})
+
+</script>
